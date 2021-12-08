b2f9b940b7c7fb34693c702aa9ad2b29ec07e4a3
Mirana <76050108+miranarkt@users.noreply.github.com>
2021-12-08 10:51:14 +0100

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ st.set_page_config(layout='wide')
 # Titre du Dashboard
 st.title("#WebFruitSummer - Cible 🍋🫐")
 
+st.write('Missionez par l\'entreprise « Extra Fruit », vous devez définir la cible pour le lancement de la campagne web #WebFruitSummer.')
+
 # Organisation du dashboard 
 left_block, right_block = st.columns([1, 1])
 
